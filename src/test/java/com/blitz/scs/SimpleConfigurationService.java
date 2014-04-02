4430e0f31eacec23f005020c32eb1329fb0b5b1d
brainysmith <brainysmith@gmail.com>
2014-04-02 22:29:26 +0400

    Merge branch 'master' of https://github.com/brainysmith/scs-lib
    Conflicts:
    	src/main/java/com/blitz/scs/SCSessionImpl.java
    	src/main/java/com/blitz/scs/service/ServiceProvider.java
    	src/main/java/com/blitz/scs/service/spi/CryptoTransformationService.java

--- src/test/java/com/blitz/scs/SimpleConfigurationService.java
+++ src/test/java/com/blitz/scs/SimpleConfigurationService.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class SimpleConfigurationService implements ConfigurationService {
 
-    private final Map<String, Long> config = new HashMap<String, Long>() {
+    private final Map<String, Object> config = new HashMap<String, Object>() {
         {
             put("com.blitz.scs.sessionMaxAgeInSec", 7 * 365 * 86400L);
         }
@@ -14,13 +14,23 @@ public class SimpleConfigurationService implements ConfigurationService {
 
     @Override
     public Long getLong(String name) {
-        return config.get(name);
+        return (Long)config.get(name);
     }
 
     @Override
     public Long getLong(String name, Long defaultValue) {
-        Long value = config.get(name);
+        Long value = (Long)config.get(name);
         return (value != null)?value:defaultValue;
+    }
+
+    @Override
+    public String getString(String name) {
+        return (String)config.get(name);
+    }
+
+    @Override
+    public String getString(String name, String defaultValue) {
+        return (String)config.get(name);
     }
 
 }
